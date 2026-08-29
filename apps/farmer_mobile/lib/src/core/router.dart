0eed1373229666922299da53910631091e7a8bae
vij7661 <58181021+vij7661@users.noreply.github.com>
2026-08-30 01:34:38 +0530

    feat: route app launch through Farmer session recovery

--- apps/farmer_mobile/lib/src/core/router.dart
+++ apps/farmer_mobile/lib/src/core/router.dart
@@ -1,29 +1,36 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../features/auth/welcome_screen.dart';
 import '../features/auth/login_screen.dart';
-import '../features/identity/register_screen.dart';
+import '../features/auth/startup_screen.dart';
+import '../features/auth/welcome_screen.dart';
+import '../features/disputes/dispute_screen.dart';
 import '../features/home/home_screen.dart';
 import '../features/identity/profile_screen.dart';
+import '../features/identity/register_screen.dart';
 import '../features/livestock/create_livestock_screen.dart';
 import '../features/marketplace/create_listing_screen.dart';
+import '../features/marketplace/listing_history_screen.dart';
 import '../features/marketplace/offers_screen.dart';
+import '../features/transaction/agreement_screen.dart';
+import '../features/transaction/settlement_screen.dart';
+import '../features/transaction/shipment_screen.dart';
 import '../features/transaction/transaction_screen.dart';
 import '../features/weighment/weighment_ack_screen.dart';
-import '../features/marketplace/listing_history_screen.dart';
-import '../features/transaction/agreement_screen.dart';
-import '../features/transaction/shipment_screen.dart';
-import '../features/disputes/dispute_screen.dart';
-import '../features/transaction/settlement_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
-    initialLocation: '/',
+    initialLocation: '/startup',
     routes: [
+      GoRoute(path: '/startup', builder: (_, __) => const StartupScreen()),
       GoRoute(path: '/', builder: (_, __) => const WelcomeScreen()),
       GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
-      GoRoute(path: '/register', builder: (_, __) => const RegisterScreen()),
+      GoRoute(
+        path: '/register',
+        builder: (_, state) => RegisterScreen(
+          resumeExisting: state.uri.queryParameters['resume'] == '1',
+        ),
+      ),
       GoRoute(path: '/home', builder: (_, __) => const HomeScreen()),
       GoRoute(path: '/profile', builder: (_, __) => const ProfileScreen()),
       GoRoute(path: '/livestock/new', builder: (_, __) => const CreateLivestockScreen()),
