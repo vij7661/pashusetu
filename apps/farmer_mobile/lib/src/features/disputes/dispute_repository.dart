ef4df3f96ab99bb97e63939cff63c8916d336847
vij7661 <58181021+vij7661@users.noreply.github.com>
2026-08-31 13:48:29 +0530

    feat: parse Farmer dispute responses

--- apps/farmer_mobile/lib/src/features/disputes/dispute_repository.dart
+++ apps/farmer_mobile/lib/src/features/disputes/dispute_repository.dart
@@ -1,17 +1,19 @@
 import '../../core/api/api_client.dart';
+import 'dispute_models.dart';
 
 class DisputeRepository {
   DisputeRepository(this._api);
   final ApiClient _api;
 
-  Future<Map<String, dynamic>> open({
+  Future<DisputeView> open({
     required String transactionId,
     required String reason,
     required int disputedAmountPaise,
-  }) {
-    return _api.post('/disputes/transactions/$transactionId', body: {
+  }) async {
+    final json = await _api.post('/disputes/transactions/$transactionId', body: {
       'reason': reason,
       'disputed_amount_paise': disputedAmountPaise,
     });
+    return DisputeView.fromJson(json);
   }
 }
